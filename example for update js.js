7f14e64dcd833f0a261f4141af54f75e9af2eeb4
edkzz <143183784+edkzz@users.noreply.github.com>
2024-01-07 16:00:58 +0300

    Add files via upload

---
+++ example for update js.js
@@ -0,0 +1,353 @@
+var fieldNameElement = document.getElementById('monday_one_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+    
+
+
+var fieldNameElement = document.getElementById('monday_two_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+
+var fieldNameElement = document.getElementById('monday_three_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('monday_four_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('monday_five_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('monday_six_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('monday_seven_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          //MONDAY | ПОНЕДЕЛЬНИК
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var fieldNameElement = document.getElementById('tuesday_one_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+    
+
+
+var fieldNameElement = document.getElementById('tuesday_two_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+
+var fieldNameElement = document.getElementById('tuesday_three_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('tuesday_four_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('tuesday_five_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('tuesday_six_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('tuesday_seven_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          //TUESDAY | ВТОРНИК
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+         
+
+
+
+
+
+var fieldNameElement = document.getElementById('wednesday_one_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+    
+
+
+var fieldNameElement = document.getElementById('wednesday_two_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+
+var fieldNameElement = document.getElementById('wednesday_three_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('wednesday_four_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('wednesday_five_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('wednesday_six_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('wednesday_seven_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          //WEDNESDAY | СРЕДА
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+         
+
+
+
+
+
+
+var fieldNameElement = document.getElementById('thursday_one_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+    
+
+
+var fieldNameElement = document.getElementById('thursday_two_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+
+var fieldNameElement = document.getElementById('thursday_three_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('thursday_four_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('thursday_five_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('thursday_six_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('thursday_seven_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          //THURSDAY | ЧЕТВЕРГ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+         
+
+
+
+
+
+var fieldNameElement = document.getElementById('friday_one_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+    
+
+
+var fieldNameElement = document.getElementById('friday_two_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+
+var fieldNameElement = document.getElementById('friday_three_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('friday_four_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('friday_five_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('friday_six_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
+
+
+var fieldNameElement = document.getElementById('friday_seven_lesson');
+        if (fieldNameElement)            
+        {fieldNameElement.innerHTML = '' ;}
